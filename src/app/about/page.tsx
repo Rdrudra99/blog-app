dc102c33178c8529aa70d5d6860cb5a27543f55a
Rdrudra99 <rdrudra99.work@gmail.com>
2024-10-19 14:52:20 +0530

    fixed build error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -29,9 +29,8 @@ export default async function AboutPage() {
             </p>
           </div>
           <p className="text-muted-foreground text-lg py-4">
-          Frontend Developer | React.js & JavaScript Enthusiast
-
-I am a passionate frontend developer with over 2 years of experience specializing in React.js and modern web technologies. I love building dynamic, user-friendly applications that solve real-world problems. My current focus is on mastering Data Structures and Algorithms to enhance my problem-solving skills and stay ahead in the ever-evolving tech landscape. When I'm not coding, you will find me exploring new frameworks or working on personal projects to expand my skills.
+            Frontend Developer | React.js & JavaScript Enthusiast
+            I am a passionate frontend developer with over 2 years of experience specializing in React.js and modern web technologies. I love building dynamic, user-friendly applications that solve real-world problems. My current focus is on mastering Data Structures and Algorithms to enhance my problem-solving skills and stay ahead in the ever-evolving tech landscape. When I am not coding, you will find me exploring new frameworks or working on personal projects to expand my skills.
           </p>
         </div>
       </div>
